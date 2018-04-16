b44eb16f27fb427c17e66258462a9f00d8e3c46e
hiennv1 <hiennv1@runsystem.net>
2018-04-16 17:36:28 +0700

    Commit Dexter

--- app/src/main/java/com/example/gmo/dexterpermission/MainActivity.java
+++ app/src/main/java/com/example/gmo/dexterpermission/MainActivity.java
@@ -1,13 +1,89 @@
 package com.example.gmo.dexterpermission;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gmo.dexterpermission.utils.PermissionUtils;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnCamera, btnLocation, btnCall, btnAll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCamera = findViewById(R.id.btn_camera);
+        btnLocation = findViewById(R.id.btn_location);
+        btnCall = findViewById(R.id.btn_call);
+        btnAll = findViewById(R.id.btn_all);
+
+        btnCamera.setOnClickListener(this);
+        btnLocation.setOnClickListener(this);
+        btnCall.setOnClickListener(this);
+        btnAll.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_camera:
+                requestCameraPermission();
+                break;
+            case R.id.btn_location:
+                requestLocationPermission();
+                break;
+            case R.id.btn_call:
+                requestCallPermission();
+                break;
+            case R.id.btn_all:
+                requestPermissionAll();
+                break;
+        }
+    }
+
+    /*Request multi permission*/
+    private void requestPermissionAll() {
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.CALL_PHONE,
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+                    }
+                });
+    }
+
+    /*Request permission call*/
+    private void requestCallPermission() {
+        if (PermissionUtils.checkPermissionCall(this)){
+
+        }
+    }
+
+    /*Request permission location*/
+    private void requestLocationPermission() {
+
+    }
+
+    /*Request permission camera*/
+    private void requestCameraPermission() {
+
     }
 }
---
+++ app/src/main/java/com/example/gmo/dexterpermission/utils/PermissionUtils.java
@@ -0,0 +1,45 @@
+package com.example.gmo.dexterpermission.utils;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Build.VERSION;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by GMO on 4/16/2018.
+ */
+
+public class PermissionUtils {
+    /*
+    *Check quyền camera*/
+    public static boolean checkPermissionCamera(Context context) {
+        if (VERSION.SDK_INT >= 23 &&
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            /*Nếu đã cấp quyền camera*/
+            return true;
+        }
+        return false;
+    }
+
+    /*Check quyền gọi*/
+    public static boolean checkPermissionCall(Context context){
+        if (VERSION.SDK_INT >= 23 &&
+                ActivityCompat.checkSelfPermission(context,Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        return false;
+    }
+
+    /*Check location*/
+    public static boolean checkPermissionLocation(Context context){
+        if (VERSION.SDK_INT >= 23 &&
+                ActivityCompat.checkSelfPermission(context,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        return false;
+    }
+
+}
