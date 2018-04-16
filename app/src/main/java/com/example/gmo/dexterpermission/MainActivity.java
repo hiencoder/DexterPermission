9d07a868c604b270c8e581f2b69b3aa1fdb9a45d
hiennv1 <hiennv1@runsystem.net>
2018-04-16 08:45:57 +0700

    Create project

---
+++ app/src/main/java/com/example/gmo/dexterpermission/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gmo.dexterpermission;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
